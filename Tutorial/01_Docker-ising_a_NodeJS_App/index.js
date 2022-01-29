a9434ae208738b1889e32b795ce364ea4d2105d8
Roman Ojha <razzroman98@gmail.com>
2022-01-29 12:45:54 +0545

    adding more stuff in dockerize nodejs tut

--- Tutorial/01_Docker-ising_a_NodeJS_App/index.js
+++ Tutorial/01_Docker-ising_a_NodeJS_App/index.js
@@ -3,8 +3,9 @@
 
     -> after completing the docker file we will use this command:
         -> docker build -t node-docker-tutorial .
-            -> to build the docker image '-t' to spacify the name of our docker image 'node-docker-tutorial' is the name of our image
-
+        -> to build the docker image '-t' to spacify the name of our docker image 'node-docker-tutorial' is the name of our image
+        -> docker build -t node-docker-tutorial:<verison_number> .
+        
     -> now from that docker image we can then start to spin up containers using:
         -> docker run -it -p 9000:8080 node-docker-tutorial
             -> how we have to run the image so:
@@ -22,26 +23,34 @@
         -> firstly we have to install nodemon as the dependencies:
             -> npm i --save nodemon
                 -> so that docker container will then have access to that package
-            -> docker run -it -p 9000:8080 -v $(pwd):/app node-docker-tutorial
+            -> docker run -it -p 9000:8080 -v ${pwd}:/app node-docker-tutorial
                 -> '-v' volume flag
+                -> ${pwd} (current working directory)
+                -> '/app' docker directory
+                -> "-v ${pwd}:/app" (is explain in depth in docker-compose.yml)
+    
+    -> Conclusion: Now this nodejs application works on any system which had installed docker 
 
     
-    => in package.json:
-        -> "docker-compose-down": "docker-compose -f './docker-compose.yml' down",
-            -> this is the script that is use to close down the container shutdown the container
-        -> "docker-compose-up:dev": "docker-compose -f './docker-compose.yml up -d --build api-dev",
-            -> this is the script that will build our container
-        -> "docker:dev": "num run docker-compose-down; npm run docker-compose-up:dev; docker-compose logs -f api-dev"
-            -> this script will run the docker container and also we want to spacify to view the logs of the container
+    => using docke-compose.yml
+        -> create docker-compose.ymal
+        -> in package.json:
+            -> "docker-compose-down": "docker-compose -f './docker-compose.yml' down",
+                -> this is the script that is use to close down the container shutdown the container
+            -> "docker-compose-up:dev": "docker-compose -f './docker-compose.yml up -d --build api-dev",
+                -> this is the script that will build our container
+            -> "docker:dev": "num run docker-compose-down; npm run docker-compose-up:dev; docker-compose logs -f api-dev"
+                -> this script will run the docker container and also we want to spacify to view the logs of the container
 */
 
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
 app.listen(8080, () => {
-  console.log("Running on 8080 PORT");
+  console.log(`Running on ${port}`);
 });
