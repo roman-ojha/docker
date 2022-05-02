625b80b530cad09121595ee0f424c7204f1595fc
Roman Ojha <razzroman98@gmail.com>
2022-05-02 10:54:59 +0545

    added more stuff for docirize nodejs and mongodb

--- Tutorial/02_dockerize_nodejs_and_mongodb/index.js
+++ Tutorial/02_dockerize_nodejs_and_mongodb/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT;
+const MongoDB_URI = process.env.MONGODB_URI;
+const DB = process.env.DB_NAME;
 const app = express();
 
 app.set("view engine", "ejs");
@@ -10,11 +14,12 @@ app.use(express.urlencoded({ extended: false }));
 // Connect to MongoDB
 mongoose
   .connect(
-    "mongodb://mongo:27017/docker-node-mongo",
+    // "mongodb://mongo:27017/docker-node-mongo",
     // to connect to mongoDB we will not use localhost rather we will sue 'mongo' because 'mongo' will going to be the name of our service that's going to be our mongo container
     // so 'mongo' name should be similar as services name
     // NOTE: while running locally on machine without docker container the 'mongo' is not working right now so it looks like we have to put 'localhost for that reason'
-    { useNewUrlParser: true }
+    MongoDB_URI,
+    { dbName: DB, useNewUrlParser: true }
   )
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
@@ -28,6 +33,12 @@ app.get("/", (req, res) => {
     .catch((err) => res.status(404).json({ msg: "No items found" }));
 });
 
+app.get("/items/get", (req, res) => {
+  Item.find()
+    .then((items) => res.json(items))
+    .catch((err) => res.status(404).json({ msg: "NO items found" }));
+});
+
 app.post("/item/add", (req, res) => {
   // we will use this route to add the item in the database
   const newItem = new Item({
@@ -37,6 +48,4 @@ app.post("/item/add", (req, res) => {
   newItem.save().then((item) => res.redirect("/"));
 });
 
-const port = 3000;
-
-app.listen(port, () => console.log("Server running..."));
+app.listen(PORT, () => console.log("Server running..."));
