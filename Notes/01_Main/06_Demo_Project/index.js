965434ead79ce2bfe01f04a775b95ae68288562b
Roman-Ojha <razzroman98@gmail.com>
2023-08-27 22:43:07 +0545

    feat: init express project with index view

---
+++ Notes/01_Main/06_Demo_Project/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = 8080;
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.sendFile(path.join(__dirname, "view/index.html"));
+});
+
+app.get("/profile-picture", (req, res) => {
+  var img = fs.readFileSync("assets/user.jpg");
+  res.writeHead(200, { "Content-Type": "image/jpg" });
+  return res.end(img, "binary");
+});
+
+app.listen(PORT, () => {
+  console.log(`running on http://localhost:${PORT}`);
+});
